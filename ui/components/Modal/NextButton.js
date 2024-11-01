5e53602e8ad8129325e17c95347ee6912142dccd
안찬웅 <1stevering@naver.com>
2024-11-01 18:35:10 +0900

    [feat/#3] : 다음 버튼 구현
    다음 버튼 구현
    
    다음버튼  공통 컴포넌트 구현
    
    dy)

---
+++ ui/components/Modal/NextButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NextButton = () => {
+  return (
+    <button className="w-[400px] h-[61px] bg-[#2483C5] text-white py-3 px-6 rounded-md hover:bg-[#1f6ea3] transition duration-300">
+      다음
+    </button>
+  );
+};
+
+export default NextButton;
