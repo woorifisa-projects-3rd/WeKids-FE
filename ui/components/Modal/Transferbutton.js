1b924b77039a46c5846395938ffe0ab22c0799be
안찬웅 <1stevering@naver.com>
2024-11-01 18:52:51 +0900

    [type/#3]: 이체 버튼 구현
    이체 버튼 구현
    
    이체 버튼 공통 컴포넌트 구현

---
+++ ui/components/Modal/Transferbutton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TransferButton = () => {
+  return (
+    <button className="w-[128px] h-[60px] flex-shrink-0 bg-[#D9D9D9] text-black py-3 px-6 rounded-md transition duration-300">
+      이체하기
+    </button>
+  );
+};
+
+export default TransferButton;
