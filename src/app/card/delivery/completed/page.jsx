f853c778b9017446e841968390121ebdfe10e4f2
안찬웅 <1stevering@naver.com>
2024-11-25 12:20:08 +0900

    [feat/#68]: 뒷면보기 버튼 생성
    뒷면보기 버튼 (아이콘, 뒷면보기, 사각형) 추가

--- src/app/card/delivery/completed/page.jsx
+++ src/app/card/delivery/completed/page.jsx
@@ -1,23 +1,30 @@
 'use client';
-
+import { ReloadIcon } from "@radix-ui/react-icons";
 import React from 'react';
-import CardCharacter from '@/src/ui/components/card-select/CardCharacter';
-import CustomButton from '@/src/ui/components/atoms/CustomButton';
+import CardCharacter from '@/src/ui/Components/card-select/CardCharacter';
+import CustomButton from '@/src/ui/Components/atoms/CustomButton';
 import Header from '@/src/ui/layout/Header';
 
 const NewComponent = () => {
     return (
-      <div className="flex flex-col items-center justify-between h-screen bg-white">
+      <div className="flex flex-col justify-between h-screen bg-white">
         <Header />
         <div className="flex-grow flex items-center justify-center">
             {/* 배경색 bg-[#57A9FB]에서 bg-MainColor로 추후 변경*/}
-            <div className="w-[313px] h-[513px] flex-shrink-0 rounded-[40px] border-2 border-black/40 bg-[#57A9FB] flex items-center justify-center">
-            <CardCharacter selectedCharacter="DADAPING" selectedColor="bg-blueDada" />
-          </div>
+            <div className="w-[313px] h-[513px] flex-shrink-0 rounded-[40px] border-2 border-black/40 bg-[#57A9FB] flex flex-col items-center justify-center">
+            <CardCharacter selectedCharacter="HEARTSPRING" selectedColor="bg-pinkHachu" />      
+                <button className="flex items-center mt-4 w-[79px] h-[26px] flex-shrink-0 rounded-[10px] bg-[#D9D9D9] text-R-10 text-black pl-1">
+                    <ReloadIcon className="mr-2 transform rotate-180 scale-x-[-1]" />
+                    뒷면보기
+                </button>
+            </div>
         </div>
         
         <div className="w-full p-4">
-          <CustomButton size="large" className="w-full" onClick={() => alert('확인 버튼 클릭')}>
+          <CustomButton 
+            size="large" 
+            className="w-full" 
+            onClick={() => alert('확인 버튼 클릭')}>
             확인
           </CustomButton>
         </div>
