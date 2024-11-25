38f9a51d8b550fb830550e69d7f414519acf66f7
안찬웅 <1stevering@naver.com>
2024-11-25 11:08:00 +0900

    [feat/#68]: 큰 사각형에  카드 캐릭터 넣는 작업

---
+++ src/app/card/delivery/completed/page.jsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React from 'react';
+import CardCharacter from '@/src/ui/Components/card-select/CardCharacter';
+import CustomButton from '@/src/ui/Components/atoms/CustomButton';
+
+const NewComponent = () => {
+    return (
+      <div className="flex flex-col items-center justify-between h-screen bg-white">
+        <div className="flex-grow flex items-center justify-center">
+            {/* 배경색 bg-[#57A9FB]에서 bg-MainColor로 추후 변경*/}
+            <div className="w-[313px] h-[513px] flex-shrink-0 rounded-[40px] border-2 border-black/40 bg-[#57A9FB] flex items-center justify-center">
+            <CardCharacter selectedCharacter="DADAPING" selectedColor="bg-blueDada" />
+          </div>
+        </div>
+        
+        <div className="w-full p-4">
+          <CustomButton size="large" className="w-full" onClick={() => alert('확인 버튼 클릭')}>
+            확인
+          </CustomButton>
+        </div>
+      </div>
+    );
+  };
+  
+  export default NewComponent;
