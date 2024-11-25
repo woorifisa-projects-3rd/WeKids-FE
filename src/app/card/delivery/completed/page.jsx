70c9aa5f7f10747c4160a3f4cc873e1924fb98ea
안찬웅 <1stevering@naver.com>
2024-11-25 11:26:13 +0900

    [feat/#68]: header 추가

--- src/app/card/delivery/completed/page.jsx
+++ src/app/card/delivery/completed/page.jsx
@@ -3,10 +3,12 @@
 import React from 'react';
 import CardCharacter from '@/src/ui/Components/card-select/CardCharacter';
 import CustomButton from '@/src/ui/Components/atoms/CustomButton';
+import Header from '@/src/ui/layout/Header';
 
 const NewComponent = () => {
     return (
       <div className="flex flex-col items-center justify-between h-screen bg-white">
+        <Header />
         <div className="flex-grow flex items-center justify-center">
             {/* 배경색 bg-[#57A9FB]에서 bg-MainColor로 추후 변경*/}
             <div className="w-[313px] h-[513px] flex-shrink-0 rounded-[40px] border-2 border-black/40 bg-[#57A9FB] flex items-center justify-center">
